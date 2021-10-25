ed6a3b568610aea2332656b9d11bda1063b8cb25
anikkumarnath <60112798+anikkumarnath@users.noreply.github.com>
2021-10-25 20:02:52 +0600

    21/10/2021
    design app bar and messenger page link with profile page

--- Android Studio Application/lib/messenger_page.dart
+++ Android Studio Application/lib/messenger_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:second_day/profilepage.dart';
 
 class Messenger_Page extends StatefulWidget {
   const Messenger_Page({Key? key}) : super(key: key);
@@ -11,6 +12,51 @@ class Messenger_Page extends StatefulWidget {
 class _Messenger_PageState extends State<Messenger_Page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context)=>ProfilePage(),
+              ),
+            );
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_outlined,
+            color: Color.fromRGBO(255, 255, 255, 1),
+          ),
+          iconSize: 20,
+        ),
+        title: Text(
+          "Messenger",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+            color: Color.fromRGBO(255, 255, 255, 1),
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context)=>Messenger_Page(),
+                ),
+              );
+            },
+            icon: Icon(
+              Icons.refresh_outlined,
+              color: Color.fromRGBO(255, 255, 255, 1),
+            ),
+            iconSize: 20,
+          ),
+        ],
+        backgroundColor: Colors.blueGrey,
+      ),
+    );
   }
 }
